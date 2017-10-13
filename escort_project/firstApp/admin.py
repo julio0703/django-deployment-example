af07983f99b5ea94262245d26f91c5038d3c5517
julio0703 <owner@owners-MacBook-Pro-2.local>
2017-10-13 14:06:07 -0500

    first

--- escort_project/firstApp/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from firstApp.models import Escort
-# Register your models here.
-admin.site.register(Escort)
--- escort_project/firstApp/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from .models import Escort
-
-class EscortForm(ModelForm):
-    class Meta:
-        model = Escort
-        fields = ['name','image','story','interest','rate']
